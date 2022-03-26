805323648c7dbedc287ac1575727aada94c4f8b2
jmcFromDelln <gitjmc@gmail.com>
2022-03-26 12:14:17 -0400

    add navbar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CourseComponent } from './course/course.component';
+import { CoursesComponent } from './courses/courses.component';
+import { TempoComponent } from './tempo/tempo.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { BooksComponent } from './books/books.component';
+import { PostsComponent } from './posts/posts.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: TempoComponent },
+  //{ path: 'courses/:name', component: CourseComponent}
+  { path: 'courses', component: CoursesComponent },
+  { path: 'course', component: CourseComponent },
+  { path: 'posts', component: PostsComponent },
+  { path: 'books', component: BooksComponent },
+  { path: '**', component: NotFoundComponent },  // Wildcard route for a 404 page
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,17 @@
+import { NotFoundComponent } from './not-found/not-found.component';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
+//import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { CoursesComponent } from './courses/courses.component';
 import { CourseComponent } from './course/course.component';
 import { PostsComponent } from './posts/posts.component';
 import { BooksComponent } from './books/books.component';
 import { TempoComponent } from './tempo/tempo.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
@@ -17,12 +20,41 @@ import { TempoComponent } from './tempo/tempo.component';
     CourseComponent,
     PostsComponent,
     BooksComponent,
-    TempoComponent
+    TempoComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule
+    // ,
+    // RouterModule.forRoot([
+    //   { 
+    //     path: '', 
+    //     component: TempoComponent
+    //   },
+    //   //{ path: 'courses/:name', component: CourseComponent}
+    //   { 
+    //     path: 'courses', 
+    //     component: CoursesComponent
+    //   },
+    //   { 
+    //     path: 'course', 
+    //     component: CourseComponent
+    //   },
+    //   { 
+    //     path: 'posts', 
+    //     component: PostsComponent
+    //   },
+    //   { 
+    //     path: 'books', 
+    //     component: BooksComponent
+    //   },
+    //   { 
+    //     path: '**', 
+    //     component: NotFoundComponent
+    //   }
+    // ])
   ],
   providers: [],
   bootstrap: [AppComponent]
