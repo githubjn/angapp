3edc514e67e97adbc1a3563cce654fac4f4296b7
jmc <gitjmc@gmail.com>
2022-03-27 09:33:14 -0400

    trying implement GA

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
 import { Component } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
+import { filter } from 'rxjs/operators';
+
+declare const gtag: Function;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'angapp-to test deploy-JMC';
+  title = 'angapp-to test deploy-JMC.';
+
+
+  constructor(private router: Router) {
+
+    this.router.events.pipe(
+      filter((event: any) => event instanceof NavigationEnd)
+    ).subscribe((event: NavigationEnd) => {
+      /** START : Code to Track Page View  */
+       gtag('event', 'page_view', {
+          page_path: event.urlAfterRedirects
+       })
+      /** END */
+    })
+
+  }
+
 }
+
